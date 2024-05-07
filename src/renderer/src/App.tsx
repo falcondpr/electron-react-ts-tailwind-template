1291d89391a863c5d5a599dfbf9269c931135e9d
Fernando Villalba <21fervillalbag@gmail.com>
2024-05-06 22:14:34 -0400

    feat: config electron with react, ts and tailwind

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+const App: React.FC = () => {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <p className="text-pink-800 text-4xl">
+        Hello <span className="text-pink-600">world</span>
+      </p>
+    </div>
+  )
+}
+
+export default App
